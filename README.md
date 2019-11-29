59166fa5e00db4afa96d62944a4bd74b92d19a6e
minamorl <minamorl@users.noreply.github.com>
2019-11-30 02:28:08 +0900

    Setup with Qt

---
+++ README.md
@@ -0,0 +1,4 @@
+```sh
+cmake .. \
+-DCMAKE_PREFIX_PATH=$HOME/Qt/5.12.6/gcc_64/
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
-int main() {
-    return 0;
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char** argv) {
+    QApplication app(argc, argv);
+    QLabel* label = new QLabel("This is the beginning of domination.");
+    label->resize(240, 120);
+    label->show();
+    return app.exec();
 }
