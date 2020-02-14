afce77c3ddf0fa4716499787adf06d2431b7479f
minamorl <minamorl@users.noreply.github.com>
2020-02-15 02:40:56 +0900

    Better shader code

--- src/colorwheel_widget.cpp
+++ src/colorwheel_widget.cpp
@@ -21,7 +21,9 @@ const GLfloat colorwheel_widget::vertices_[] = {
 };
 
 const char* colorwheel_widget::vertex_shader_ = R"(
-attribute highp vec4 vertex;
+#version 440 core
+
+in vec4 vertex;
 
 void main(){
     gl_Position = vertex;
@@ -29,17 +31,12 @@ void main(){
 )";
 
 const char* colorwheel_widget::fragment_shader_ = R"(
-#ifdef GL_ES
-precision mediump float;
-#endif
+#version 440 core
 
-#define TWO_PI 6.28318530718
-
-#extension GL_OES_standard_derivatives : enable
-
-// uniform float time;
-// uniform vec2 mouse;
 uniform vec2 resolution;
+layout(location = 0) out vec4 FragColor;
+
+const float TWO_PI = 6.28318530718;
 
 vec3 hsv_to_rgb( in vec3 hsv ) {
 	vec3 v1 = vec3(5.0, 3.0, 1.0);
@@ -50,16 +47,20 @@ vec3 hsv_to_rgb( in vec3 hsv ) {
 	return result;	
 }
 
-void main( void ) {
-	vec2 position = gl_FragCoord.xy / resolution.x;
+void main() {
+	vec2 position = gl_FragCoord.xy / resolution.y;
 	vec2 to_center = vec2(0.5) - position;
 	float angle = (atan(to_center.x, to_center.y) / TWO_PI + 0.5) * 360.0;
 	float len = length(to_center);
 
 	vec3 col = hsv_to_rgb(vec3(angle, 1.0, 1.0));
 	
-	gl_FragColor = vec4(col, 1.0);
+   	if (len < 0.4 || len > 0.5)
+        FragColor = vec4(0, 0, 0, 0);
+    else
+		FragColor = vec4(col, 1.0);
 }
+
 )";
 
 colorwheel_widget::colorwheel_widget(QWidget* parent)
